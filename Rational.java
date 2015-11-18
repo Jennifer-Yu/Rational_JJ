d3448bd5e486921f48bf19bd500d8c470e7917a5
Jackson Dey <Jackson@Jacksons-MacBook-Pro-2.local>
2015-11-17 22:07:48 -0500

    Alrighty

--- Rational.java
+++ Rational.java
@@ -1,4 +1,41 @@
 public class Rational {
+    
+
+//Instance variables for the numerator and denominator
+
+
+//Default constructor (no parameters)
+
+
+//Creates a new Rational with the value of 0/1
+
+/*Constructor takes 2 parameters, one for the numerator, one for the denominator
+if an invalid denominator is attempted, should print a message and set the number to 0/1 */
+
+//toString - returns a string representation of the rational number (formatting of your choice)
+    
+/*
+floatValue - Returns a floating point value of the number, Uses the most precise floating point primitive
+multiply
+*/
+
+    
+/*
+Takes 1 Rational object as a parameter and multiplies it by this Rational object
+Does not return any value 
+Should modify this object and leave the parameter alone (see below for example)
+Need not reduce the fraction
+*/
+
+    
+/*
+divide
+Works the same as multiply, except the operation is division
+*/
+     
+
+
+    
     public static void main(String[] args) {
 	System.out.println ("hi");
     }
